dc7d4497888a10a1867a4aa13002ccb3f835f010
HannaObeid <hanna_s_obeid@hotmail.com>
2022-11-07 17:27:37 +0100

    visual lib progress

---
+++ visual_processing_lib/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='visual_processing_lib',
+      version='0.1',
+      description='Visual Processing Library',
+      url='',
+      author='Antedeguemon',
+      author_email='',
+      license='MIT',
+      packages=['visual_processing_lib'],
+      zip_safe=False)
