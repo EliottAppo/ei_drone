c86a0277fa4b29e459fa35f91fb6cdc9d8d68bc4
Igor Oliveira <igor.oliveira@student-cs.fr>
2022-11-07 16:40:08 +0100

    ok

--- ros_packages/low_level/nodes/teleop_joy.py
+++ ros_packages/low_level/nodes/teleop_joy.py
@@ -3,7 +3,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-
+from std_msgs.msg import Empty
 
 class TeleopJoy:
     """Joystick teleoperation."""
@@ -13,28 +13,33 @@ class TeleopJoy:
         # publisher
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel',
                                            Twist, queue_size=1)
+        self.takeoff_pub = rospy.Publisher('/bebop/takeoff', Empty, queue_size=1)
+        self.land_pub = rospy.Publisher('/bebop/land', Empty, queue_size=1)
         # subscriber
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_cb)
-
+        
+        self.flying = False
     def joy_cb(self, msg):
         """Compute and publish twist command."""
         # initialize message
         cmd_vel_msg = Twist()
         # fill message
+        
         if msg.buttons[7] > .5:
-            cmd_vel_msg.linear.x = .5 * msg.axes[1]
-            cmd_vel_msg.linear.y = .5 * msg.axes[2]
-            cmd_vel_msg.linear.z = .5 * msg.axes[3]
-            cmd_vel_msg.angular.x = 0
-            cmd_vel_msg.angular.y = 0
-            cmd_vel_msg.angular.z =  msg.axes[0]
+            if not self.flying:
+                takeoff_pub.publish(Empty())
+                self.flying = True
+            else:
+
+                cmd_vel_msg.linear.x = .5 * msg.axes[1]
+                cmd_vel_msg.linear.y = .5 * msg.axes[2]
+                cmd_vel_msg.linear.z = .5 * msg.axes[3]
+                cmd_vel_msg.angular.x = 0
+                cmd_vel_msg.angular.y = 0
+                cmd_vel_msg.angular.z =  msg.axes[0]
         else :
-            cmd_vel_msg.linear.x = 0
-            cmd_vel_msg.linear.y = 0 
-            cmd_vel_msg.linear.z = 0
-            cmd_vel_msg.angular.x = 0
-            cmd_vel_msg.angular.y = 0
-            cmd_vel_msg.angular.z = 0
+            land.publish(Empty())
+            self.flying = False
         # publish message
         self.cmd_vel_pub.publish(cmd_vel_msg)
 
