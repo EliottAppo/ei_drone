c38e2fc460c2598276ce4f11e15fe3cd018d1044
Eliott Dumont <eliott.dumont@student-cs.fr>
2022-11-08 11:16:02 +0100

    ok

--- ros_packages/low_level/nodes/speed_controller.py
+++ ros_packages/low_level/nodes/speed_controller.py
@@ -2,32 +2,77 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Empty
+from std_msgs.msg import Float32, Empty
+from nav_msgs.msg import Odometry
+from multiprocessing import Lock
+import numpy as np
 
 class SpeedController:
 
     def __init__(self):
+        self.x_vel_max = get_param()
+
 
         #Publisher
         self.gathered_vel_pub = rospy.Publisher('/target_vel', Twist, queue_size=1)
-
+        
         #Subscriber
-        self.linear_x_sub = rospy.Subscriber('/linear_x', , self.linear_x_cb)
-        self.linear_y_sub = rospy.Subscriber('/linear_y', Twist, self.linear_y_cb)
-        self.linear_z_sub = rospy.Subscriber('/linear_z', Twist, self.linear_z_cb)
-        self.angular_z = rospy.Subscriber('/angular_z', Twist, self.angular_z_cb)
+        self.linear_x_sub = rospy.Subscriber('/linear_x', Float32, self.linear_x_cb)
+        self.linear_y_sub = rospy.Subscriber('/linear_y', Float32, self.linear_y_cb)
+        self.linear_z_sub = rospy.Subscriber('/linear_z', Float32, self.linear_z_cb)
+        self.angular_z = rospy.Subscriber('/angular_z', Float32, self.angular_z_cb)
 
+        self.hover_mode = rospy.Subscriber('/hover', Empty, self.hover_cb)
+
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_cb)
+
+        #Variables
+        self.gathered_vel_msg = Twist()
+        self.gathered_vel_msg_mutex = Lock()
+
+    def odom_cb(self, msg):
+        current_velocity = msg.twist.twist
+
+    def hover_cb(self, msg):
+       
+        with self.gathered_vel_msg_mutex:
+            self.gathered_vel_msg = Twist()
+        
     def linear_x_cb(self, msg):
+       
+        with self.gathered_vel_msg_mutex:
+            self.gathered_vel_msg.linear.x = msg.data  
+
     def linear_y_cb(self, msg):
+        
+        with self.gathered_vel_msg_mutex:
+            self.gathered_vel_msg.linear.y = msg.data  
     def linear_z_cb(self, msg):
-        linear_z_gathered = msg.data
-        self.cmd_vel_pub.publish(cmd_vel_msg)
+      
+        with self.gathered_vel_msg_mutex:
+            self.gathered_vel_msg.linear.z = msg.data       
 
     def angular_z_cb(self, msg):
-        angular_z_gathered = msg
+       
+        with self.gathered_vel_msg_mutex:
+            self.gathered_vel_msg.angular.z = msg.data
+
+    def loop(self):
+        while not rospy.is_shutdown():
+            with self.gathered_vel_msg_mutex:
+                self.gathered_vel_output = Twist()
+                self.gathered_vel_output.linear.z = self.gathered_vel_msg.linear.z
+                self.gathered_vel_output.angular.z = self.gathered_vel_msg.angular.z
+                self.gathered_vel_pub.publish(self.gathered_vel_output)
 
 def main():
+    """Instantiate node and class."""
+    # declare node
     rospy.init_node('speed_controller')
+    # instantiate class
+    speed_controller = SpeedController()
+    speed_controller.loop()
+   
 
     
 
